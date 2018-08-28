b6fedf4919b1146c88d91e9559a3d5dfcab2765d
mandeep2809 <mandeepkandhari@gmail.com>
2018-08-28 03:06:25 -0400

    Adding Back button to Sign in page

--- src/Signin.js
+++ src/Signin.js
@@ -41,20 +41,26 @@ class Signin extends Component {
   }
   render(){
       return(
-    <article className="br4 shadow-5 ba mh5 dark-gray mv5 w-80 w-50-m w-60-l flex justify-center center">
-    <main className="pa4 black-80 w-70">
-  <div className="measure">
-    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-      <legend className="f2-l f4 fw6 w-100 tc center">Sign In</legend>
-      <div className="mt3">
-        <label className="db fw6 lh-copy flex items-start f6 w-100" htmlFor="email-address">Email</label>
-        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-          type="email" 
-          name="email-address"
-          id="email-address"
-          onChange={this.onEmailChange} 
-        />
+    <div>   
+    <nav>
+      <div className="pr1 tr">
+          <p onClick={()=>this.props.onRouteChange('Products')} className="link pointer dim dark-gray f2 fw6 f5-ns dib mr3 mr4-ns">Back to Home Page</p>
       </div>
+    </nav> 
+    <article className="br4 shadow-5 ba mh5 dark-gray mv5 w-80 w-50-m w-60-l flex justify-center center">
+      <main className="pa4 black-80 w-70">
+        <div className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+          <legend className="f2-l f4 fw6 w-100 tc center">Sign In</legend>
+          <div className="mt3">
+          <label className="db fw6 lh-copy flex items-start f6 w-100" htmlFor="email-address">Email</label>
+          <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+            type="email" 
+            name="email-address"
+            id="email-address"
+            onChange={this.onEmailChange} 
+          />
+        </div>
       <div className="mv3">
         <label className="db fw6 lh-copy f6 flex items-start" htmlFor="password">Password</label>
         <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
@@ -65,21 +71,21 @@ class Signin extends Component {
         />
       </div>
       
-    </fieldset>
-    <div className="lh-copy mt3 flex justify-center">
+      </fieldset>
+      <div className="lh-copy mt3 flex justify-center">
       <input className="b ph3 w-40-l w-40-m w-100 tc pv2 input-reset ba b--black bg-transparent grow pointer fw6 f6 dib" 
       type="submit" value="Sign in" onClick={this.onSubmitSignin}/>
-    </div>
-    <div>
+      </div>
+      <div>
       {this.state.error === 'error logging in'? <p className='f5 fw6 red'>Either the Email or the password is incorrect</p>:null}
-    </div>
-    <div className="lh-copy mt3">
+      </div>
+      <div className="lh-copy mt3">
       <p className="f5 fw6 link pointer dim black tc db pointer" onClick={()=>this.props.onRouteChange('Register')}>Register</p>
-      
-    </div>
+      </div>
   </div>
 </main>
 </article>
+</div>
     );
   }
 }
